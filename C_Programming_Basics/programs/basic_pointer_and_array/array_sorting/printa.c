5517d85b642d4ef9c6464d769a6bc72f64e8d3e7
revant.koriya <revant.koriya@volansys.com>
2024-01-30 17:08:31 +0530

    new files added

---
+++ C_Programming_Basics/programs/basic_pointer_and_array/array_sorting/printa.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int printa(int* arr,l)
+{
+    for(int i =0;i<l;i++)
+    {
+        printf("%d ",arr[i]); 
+    }    
+}
---
+++ C_Programming_Basics/programs/basic_pointer_and_array/array_sorting/reverse.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int reverse(arr[int n]);
+int main()
+{
+    printf("\n********************************************\n");
+    int n;
+    printf("enter the value\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter value :");
+    for(int i =0 ;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    reverse(arr[n]);
+        printf("\n********************************************\n");
+    return 0;
+}
+
+int reverse(arr[int n])
+{
+    for(int i =0;i/2<n;i++)
+    {
+       int arr[n];
+       arr[n]=arr[i];
+       arr
+    }
+}
+
